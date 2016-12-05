f223a8830ebd56631366d72b4493c289825975c2
wth8647 <wth8647@gmail.com>
2016-12-05 20:30:10 +0700

    search function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -30,4 +30,14 @@ class StaticPagesController < ApplicationController
 
   def help
   end
+
+  def search
+    @q = Product.ransack(params[:q])
+    @products = @q.result(distinct: true)
+  end
+
+  # def index
+  #   @q = Person.ransack(params[:q])
+  #   @people = @q.result(distinct: true)
+  # end
 end
